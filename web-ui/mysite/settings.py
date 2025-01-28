4275aa979aaeac883427e709e11ef09fde1bc9c6
Dawid <83449241+obrocki@users.noreply.github.com>
2025-01-28 11:04:17 +0000

    bump version to 0.0.4, update Python requirement, and modify dependencies in pyproject.toml; switch database to SQLite in settings.py; clean up evaluation notebook

--- web-ui/mysite/settings.py
+++ web-ui/mysite/settings.py
@@ -78,19 +78,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     # LOCAL SQLite
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # } 
-    # AZURE POSTGRES
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    } 
+    # AZURE POSTGRES
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('DB_NAME'),
+    #     'USER': os.getenv('DB_USER'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD'),
+    #     'HOST': os.getenv('DB_HOST'),
+    #     'PORT': os.getenv('DB_PORT'),
+    #     'OPTIONS': {
+    #         'sslmode': 'require',
+    #     },
+    # }
 }
 
 # Password validation
